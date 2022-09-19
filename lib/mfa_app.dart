283cb1a189f5fb075621d364864d67f8f8542736
Misha Soliterman <msoliter@gmail.com>
2022-09-19 23:31:37 +0900

    factor our Flutter hooks

--- lib/mfa_app.dart
+++ lib/mfa_app.dart
@@ -1,8 +1,7 @@
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:mfa_app/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 
-class MfaApp extends HookWidget {
+class MfaApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,75 +1,106 @@
-import 'package:mfa_app/hooks/use_user.dart';
+import 'dart:async';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:mfa_app/utilities/mfa.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 
-class LoginScreen extends HookWidget {
+class LoginScreenState extends State<LoginScreen> {
+  final StreamController<User?> userController = StreamController();
+
+  final TextEditingController emailController = TextEditingController();
+
+  final TextEditingController passwordController = TextEditingController();
+
+  final TextEditingController phoneNumberController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    userController.addStream(FirebaseAuth.instance.authStateChanges());
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    userController.close();
+    emailController.dispose();
+    passwordController.dispose();
+    phoneNumberController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    final user = useCurrentUser();
-    final emailController = useTextEditingController();
-    final passwordController = useTextEditingController();
-    final phoneNumberController = useTextEditingController();
+    return StreamBuilder<User?>(
+      builder: (context, snapshot) {
+        final user = snapshot.data;
 
-    return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(32),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Email'),
-              TextField(controller: emailController),
-              SizedBox(height: 16),
-              Text('Password'),
-              TextField(
-                controller: passwordController,
-                obscureText: true,
-              ),
-              SizedBox(height: 16),
-              TextField(controller: phoneNumberController),
-              SizedBox(height: 4),
-              Text('Phone number is only used for the initial login.'),
-              SizedBox(height: 16),
-              ElevatedButton(
-                onPressed: () async {
-                  try {
-                    final mfa = await login(
-                      email: emailController.text,
-                      password: passwordController.text,
-                    );
-
-                    await mfa.when(
-                      enrollment: (enrollment) => enrollMFA(
-                        context: context,
-                        enrollment: enrollment,
-                        phoneNumber: phoneNumberController.text,
-                      ),
-                      verification: (verification) => verifyMFA(
-                        context: context,
-                        verification: verification,
-                      ),
-                    );
-                  } catch (error) {
-                    showDialog(
-                      context: context,
-                      builder: (context) {
-                        return AlertDialog(
-                          title: Text('Login Error'),
-                          content: Text(error.toString()),
+        return Scaffold(
+          body: Padding(
+            padding: EdgeInsets.all(32),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Email'),
+                  TextField(controller: emailController),
+                  SizedBox(height: 16),
+                  Text('Password'),
+                  TextField(
+                    controller: passwordController,
+                    obscureText: true,
+                  ),
+                  SizedBox(height: 16),
+                  TextField(controller: phoneNumberController),
+                  SizedBox(height: 4),
+                  Text('Phone number is only used for the initial login.'),
+                  SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: () async {
+                      try {
+                        final mfa = await login(
+                          email: emailController.text,
+                          password: passwordController.text,
                         );
-                      },
-                    );
-                  }
-                },
-                child: Text('Login'),
+
+                        await mfa.when(
+                          enrollment: (enrollment) => enrollMFA(
+                            context: context,
+                            enrollment: enrollment,
+                            phoneNumber: phoneNumberController.text,
+                          ),
+                          verification: (verification) => verifyMFA(
+                            context: context,
+                            verification: verification,
+                          ),
+                        );
+                      } catch (error) {
+                        showDialog(
+                          context: context,
+                          builder: (context) {
+                            return AlertDialog(
+                              title: Text('Login Error'),
+                              content: Text(error.toString()),
+                            );
+                          },
+                        );
+                      }
+                    },
+                    child: Text('Login'),
+                  ),
+                  SizedBox(height: 16),
+                  Text('Current user: $user'),
+                ],
               ),
-              SizedBox(height: 16),
-              Text('Current user: $user'),
-            ],
+            ),
           ),
-        ),
-      ),
+        );
+      },
     );
+  }
+}
+
+class LoginScreen extends StatefulWidget {
+  @override
+  State<LoginScreen> createState() {
+    return LoginScreenState();
   }
 }
